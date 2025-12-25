a7b55e90bf371c5dc0bad9ad812ea4dd62067c11
vjratech <vjratechnologies@gmail.com>
2025-12-26 00:14:20 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+      }
+    });
+  },
+  { threshold: 0.15 }
+);
+
+reveals.forEach(section => {
+  observer.observe(section);
+});
